ce13a2f98f252b175c4de0913c51be5129554e51
Nat Budin <natbudin@gmail.com>
2020-03-05 08:07:20 -0800

    Put neilhosting.net on the same settings as interconlarp.org

--- neilhosting.net.tf
+++ neilhosting.net.tf
@@ -50,12 +50,26 @@ resource "aws_route53_record" "neilhosting_net_amazonses" {
   zone_id = aws_route53_zone.neilhosting_net.zone_id
   name = "_amazonses.neilhosting.net"
   type = "TXT"
-  ttl = 300
-  reords = ["LEgL3GeA6W0aInKMomh4M7hIGmfNgWSTPYDZBKtqOtk="]
+  ttl = 1800
+  records = ["LEgL3GeA6W0aInKMomh4M7hIGmfNgWSTPYDZBKtqOtk="]
 }
 
 resource "aws_acm_certificate" "neilhosting_net" {
   domain_name = "neilhosting.net"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "neilhosting_net_cert_validation" {
+  name    = aws_acm_certificate.neilhosting_net.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.neilhosting_net.domain_validation_options.0.resource_record_type
+  zone_id = aws_route53_zone.neilhosting_net.zone_id
+  records = [aws_acm_certificate.neilhosting_net.domain_validation_options.0.resource_record_value]
+  ttl     = 300
+}
+
+resource "aws_acm_certificate_validation" "neilhosting_net" {
+  certificate_arn         = aws_acm_certificate.neilhosting_net.arn
+  validation_record_fqdns = [aws_route53_record.neilhosting_net_cert_validation.fqdn]
 }
 
 resource "aws_cloudfront_distribution" "neilhosting_net" {
@@ -80,7 +94,7 @@ resource "aws_cloudfront_distribution" "neilhosting_net" {
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = local.s3_bucket_origin
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
       headers = []
@@ -89,6 +103,12 @@ resource "aws_cloudfront_distribution" "neilhosting_net" {
       cookies {
         forward = "none"
       }
+    }
+
+    lambda_function_association {
+      event_type = "origin-response"
+      include_body = false
+      lambda_arn = aws_lambda_function.addSecurityHeaders.qualified_arn
     }
   }
 
