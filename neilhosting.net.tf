3fb20a3306677fe954ec6f880d2761e3d43430ae
Nat Budin <natbudin@gmail.com>
2020-03-05 07:43:31 -0800

    Start adding DNS records

--- neilhosting.net.tf
+++ neilhosting.net.tf
@@ -11,8 +11,51 @@ locals {
   s3_bucket_origin = "S3-neilhosting.net"
 }
 
-resource "aws_acm_certificate" "neilhosting_net" {
+resource "aws_route53_zone" "neilhosting_net" {
+  name = "neilhosting.net"
+}
 
+resource "aws_route53_record" "neilhosting_net_alias" {
+  zone_id = aws_route53_zone.neilhosting_net.zone_id
+  name = "neilhosting.net"
+  type = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.neilhosting_net.domain_name
+    zone_id = aws_cloudfront_distribution.neilhosting_net.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "neilhosting_net_mx" {
+  zone_id = aws_route53_zone.neilhosting_net.zone_id
+  name = "neilhosting.net"
+  type = "MX"
+  ttl = 300
+  records = [
+    "10 mxa.mailgun.org.",
+    "10 mxb.mailgun.org."
+  ]
+}
+
+resource "aws_route53_record" "neilhosting_net_spf" {
+  zone_id = aws_route53_zone.neilhosting_net.zone_id
+  name = "neilhosting.net"
+  type = "TXT"
+  ttl = 300
+  records = ["v=spf1 include:mailgun.org ~all"]
+}
+
+resource "aws_route53_record" "neilhosting_net_amazonses" {
+  zone_id = aws_route53_zone.neilhosting_net.zone_id
+  name = "_amazonses.neilhosting.net"
+  type = "TXT"
+  ttl = 300
+  reords = ["LEgL3GeA6W0aInKMomh4M7hIGmfNgWSTPYDZBKtqOtk="]
+}
+
+resource "aws_acm_certificate" "neilhosting_net" {
+  domain_name = "neilhosting.net"
 }
 
 resource "aws_cloudfront_distribution" "neilhosting_net" {
