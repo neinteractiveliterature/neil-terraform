e3ed408dfc4bab2b094d568dd14deca91d312002
Nat Budin <natbudin@gmail.com>
2020-03-13 13:20:37 -0700

    logging for email sends and receives

--- ses-receiving.tf
+++ ses-receiving.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "intercode_inbox" {
   bucket = "intercode-inbox"
+  acl = "private"
 
   policy = <<EOF
 {
@@ -22,10 +23,103 @@ resource "aws_s3_bucket" "intercode_inbox" {
   ]
 }
   EOF
+
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = 14
+    }
+  }
+}
+
+resource "aws_iam_role" "sns_success_feedback" {
+  assume_role_policy    = jsonencode(
+    {
+      Statement = [
+        {
+          Action    = "sts:AssumeRole"
+          Effect    = "Allow"
+          Principal = {
+            Service = "sns.amazonaws.com"
+          }
+        },
+      ]
+      Version   = "2012-10-17"
+    }
+  )
+}
+
+resource "aws_iam_role_policy" "sns_success_feedback" {
+  name   = "oneClick_SNSSuccessFeedback_1584129549314"
+  role   = aws_iam_role.sns_success_feedback.name
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action   = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "logs:PutMetricFilter",
+            "logs:PutRetentionPolicy",
+          ]
+          Effect   = "Allow"
+          Resource = [
+            "*",
+          ]
+        },
+      ]
+      Version   = "2012-10-17"
+    }
+  )
+}
+
+resource "aws_iam_role" "sns_failure_feedback" {
+  assume_role_policy    = jsonencode(
+    {
+      Statement = [
+        {
+          Action    = "sts:AssumeRole"
+          Effect    = "Allow"
+          Principal = {
+            Service = "sns.amazonaws.com"
+          }
+        },
+      ]
+      Version   = "2012-10-17"
+    }
+  )
+}
+
+resource "aws_iam_role_policy" "sns_failure_feedback" {
+  name   = "oneClick_SNSFailureFeedback_1584129549315"
+  role   = aws_iam_role.sns_failure_feedback.name
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action   = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "logs:PutMetricFilter",
+            "logs:PutRetentionPolicy",
+          ]
+          Effect   = "Allow"
+          Resource = [
+            "*",
+          ]
+        },
+      ]
+      Version   = "2012-10-17"
+    }
+  )
 }
 
 resource "aws_sns_topic" "intercode_inbox_deliveries" {
   name = "intercode-inbox-deliveries"
+  http_success_feedback_role_arn = aws_iam_role.sns_success_feedback.arn
+  http_failure_feedback_role_arn = aws_iam_role.sns_failure_feedback.arn
 }
 
 resource "aws_sns_topic_subscription" "intercode_inbox_deliveries_webhook" {
@@ -69,5 +163,22 @@ resource "aws_ses_receipt_rule" "store_and_notify" {
     position = 1
     kms_key_arn = "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:alias/aws/ses"
     topic_arn = aws_sns_topic.intercode_inbox_deliveries.arn
+  }
+}
+
+resource "aws_ses_configuration_set" "default" {
+  name = "default"
+}
+
+resource "aws_ses_event_destination" "cloudwatch" {
+  name                   = "ses-sends"
+  configuration_set_name = aws_ses_configuration_set.default.name
+  enabled                = true
+  matching_types         = ["bounce", "complaint", "delivery", "reject", "send"]
+
+  cloudwatch_destination {
+    default_value  = aws_ses_configuration_set.default.name
+    dimension_name = "ses:configuration-set"
+    value_source   = "messageTag"
   }
 }
