ce3b239895c31becf278404e0c6eb9fc03726100
Nat Budin <natbudin@gmail.com>
2022-05-06 11:50:03 -0700

    Bring the CloudFlare zone under terraform management

--- extraconlarp.org.tf
+++ extraconlarp.org.tf
@@ -1,15 +1,18 @@
 locals {
-  cloudflare_zone_id = "da83fa981604d240175058922e531964"
   convention_subdomains = toset([
     "2021"
   ])
+}
+
+resource "cloudflare_zone" "extraconlarp_org" {
+  zone = "extraconlarp.org"
 }
 
 module "extraconlarp_org_cloudfront" {
   source = "./modules/cloudfront_apex_redirect"
 
   cloudflare_zone = {
-    zone_id : local.cloudflare_zone_id,
+    zone_id : cloudflare_zone.extraconlarp_org.id,
     name : "extraconlarp.org"
   }
   redirect_destination     = "https://2021.extraconlarp.org"
@@ -18,7 +21,7 @@ module "extraconlarp_org_cloudfront" {
 }
 
 resource "cloudflare_record" "extraconlarp_org_mx" {
-  zone_id  = local.cloudflare_zone_id
+  zone_id  = cloudflare_zone.extraconlarp_org.id
   name     = "extraconlarp.org"
   type     = "MX"
   value    = "inbound-smtp.us-east-1.amazonaws.com"
@@ -26,21 +29,21 @@ resource "cloudflare_record" "extraconlarp_org_mx" {
 }
 
 resource "cloudflare_record" "extraconlarp_org_wildcard_cname" {
-  zone_id = local.cloudflare_zone_id
+  zone_id = cloudflare_zone.extraconlarp_org.id
   name    = "*"
   type    = "CNAME"
   value   = "neilhosting.onrender.com"
 }
 
 resource "cloudflare_record" "extraconlarp_org_acme_challenge_cname" {
-  zone_id = local.cloudflare_zone_id
+  zone_id = cloudflare_zone.extraconlarp_org.id
   name    = "_acme-challenge"
   type    = "CNAME"
   value   = "neilhosting.verify.renderdns.com"
 }
 
 resource "cloudflare_record" "extraconlarp_org_cf_custom_hostname_cname" {
-  zone_id = local.cloudflare_zone_id
+  zone_id = cloudflare_zone.extraconlarp_org.id
   name    = "_cf-custom-hostname"
   type    = "CNAME"
   value   = "neilhosting.hostname.renderdns.com"
@@ -49,7 +52,7 @@ resource "cloudflare_record" "extraconlarp_org_cf_custom_hostname_cname" {
 resource "cloudflare_record" "extraconlarp_org_convention_subdomain_cname" {
   for_each = local.convention_subdomains
 
-  zone_id = local.cloudflare_zone_id
+  zone_id = cloudflare_zone.extraconlarp_org.id
   name    = each.value
   type    = "CNAME"
   value   = "neilhosting.onrender.com"
@@ -58,7 +61,7 @@ resource "cloudflare_record" "extraconlarp_org_convention_subdomain_cname" {
 resource "cloudflare_record" "extraconlarp_org_convention_subdomain_mx" {
   for_each = local.convention_subdomains
 
-  zone_id  = local.cloudflare_zone_id
+  zone_id  = cloudflare_zone.extraconlarp_org.id
   name     = each.value
   type     = "MX"
   value    = "inbound-smtp.us-east-1.amazonaws.com"
@@ -68,7 +71,7 @@ resource "cloudflare_record" "extraconlarp_org_convention_subdomain_mx" {
 resource "cloudflare_record" "extraconlarp_org_convention_subdomain_events_mx" {
   for_each = local.convention_subdomains
 
-  zone_id  = local.cloudflare_zone_id
+  zone_id  = cloudflare_zone.extraconlarp_org.id
   name     = "events.${each.value}"
   type     = "MX"
   value    = "inbound-smtp.us-east-1.amazonaws.com"
@@ -78,21 +81,21 @@ resource "cloudflare_record" "extraconlarp_org_convention_subdomain_events_mx" {
 resource "cloudflare_record" "extraconlarp_org_amazonses_dkim_record" {
   count = 3
 
-  zone_id = local.cloudflare_zone_id
+  zone_id = cloudflare_zone.extraconlarp_org.id
   name    = "${element(aws_ses_domain_dkim.extraconlarp_org.dkim_tokens, count.index)}._domainkey"
   type    = "CNAME"
   value   = "${element(aws_ses_domain_dkim.extraconlarp_org.dkim_tokens, count.index)}.dkim.amazonses.com"
 }
 
 resource "cloudflare_record" "extraconlarp_org_spf_record" {
-  zone_id = local.cloudflare_zone_id
+  zone_id = cloudflare_zone.extraconlarp_org.id
   name    = "extraconlarp.org"
   type    = "TXT"
   value   = "v=spf1 include:amazonses.com ~all"
 }
 
 resource "cloudflare_record" "extraconlarp_org_google_site_verification_record" {
-  zone_id = local.cloudflare_zone_id
+  zone_id = cloudflare_zone.extraconlarp_org.id
   name    = "extraconlarp.org"
   type    = "TXT"
   value   = "google-site-verification=FD3Na7QpetcgjXgnJAQUBTwqmyi9oh8LsZ34ODrLuUM"
