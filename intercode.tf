b4e9ec92e315ba1966b74f3ca55fbeaafc2452c4
Nat Budin <nbudin@actbluetech.com>
2023-01-11 15:23:59 -0800

    yjit

--- intercode.tf
+++ intercode.tf
@@ -102,6 +102,7 @@ resource "heroku_app" "intercode" {
     RAILS_SERVE_STATIC_FILES            = "enabled"
     ROLLBAR_CLIENT_ACCESS_TOKEN         = rollbar_project_access_token.intercode_post_client_item.access_token
     ROLLBAR_PUBLIC_PATH                 = "//neilhosting.net/packs"
+    RUBY_YJIT_ENABLE                    = "1"
     TWILIO_SMS_NUMBER                   = "+14156345010"
     UPLOADS_HOST                        = "https://uploads.neilhosting.net"
     WEB_CONCURRENCY                     = "0"
