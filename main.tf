95e8a188c1802e0b93ad56acab5a68d687037f27
Nat Budin <nbudin@actbluetech.com>
2025-03-16 10:46:00 -0700

    Set up Github OIDC provider

--- main.tf
+++ main.tf
@@ -93,3 +93,14 @@ resource "aws_s3_bucket_versioning" "neil-terraform-state" {
     status = "Enabled"
   }
 }
+
+module "github-oidc" {
+  source  = "terraform-module/github-oidc-provider/aws"
+  version = "~> 1"
+
+  create_oidc_provider = true
+  create_oidc_role     = true
+
+  repositories              = ["neinteractiveliterature/neil-terraform"]
+  oidc_role_attach_policies = []
+}
