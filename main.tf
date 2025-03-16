6f892ce5aeabbad3eb18ce49db1064d59c214f05
Nat Budin <nbudin@actbluetech.com>
2025-03-16 10:50:10 -0700

    Add role ARN

--- main.tf
+++ main.tf
@@ -104,3 +104,13 @@ module "github-oidc" {
   repositories              = ["neinteractiveliterature/neil-terraform"]
   oidc_role_attach_policies = []
 }
+
+output "oidc_provider_arn" {
+  description = "OIDC provider ARN"
+  value       = module.github-oidc.oidc_provider_arn
+}
+
+output "github_oidc_role" {
+  description = "CICD GitHub role."
+  value       = module.github-oidc.oidc_role
+}
