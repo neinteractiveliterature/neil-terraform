991b7739bac3c4ab8cd5955ca616d865ca4cf7ce
Nat Budin <nbudin@actbluetech.com>
2026-02-08 18:45:12 -0800

    Refactor SST deploy stuff

--- interactiveliterature.org.tf
+++ interactiveliterature.org.tf
@@ -331,138 +331,17 @@ resource "github_repository" "www_interactiveliterature_org" {
   vulnerability_alerts = true
 }
 
-resource "aws_iam_role" "www_interactiveliterature_org_deploy" {
-  name = "www_interactiveliterature_org_deploy"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+module "www_interactiveliterature_org_sst_github_deployment" {
+  source = "./modules/sst_github_deployment"
 
-    Statement = [{
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Effect = "Allow"
-
-      Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" : "repo:${github_repository.www_interactiveliterature_org.full_name}:*"
-        }
-      }
-
-      Principal = {
-        Federated = module.github-oidc.oidc_provider_arn
-      }
-    }]
-  })
+  app_name = "neil-website"
+  cloudflare_account_id = cloudflare_account.neil.id
+  github_repository = github_repository.www_interactiveliterature_org
+  oidc_provider_arn = module.github-oidc.oidc_provider_arn
+  writable_cloudflare_zones = [cloudflare_zone.interactiveliterature_org]
 }
 
-resource "aws_iam_role_policy" "www_interactiveliterature_org_deploy" {
-  role = aws_iam_role.www_interactiveliterature_org_deploy.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        "Sid" : "ManageBootstrapStateBucket",
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:CreateBucket",
-          "s3:PutBucketVersioning",
-          "s3:PutBucketNotification",
-          "s3:DeleteObject",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:PutObject"
-        ],
-        "Resource" : [
-          "arn:aws:s3:::sst-state-*"
-        ]
-      },
-      {
-        "Sid" : "ManageBootstrapAssetBucket",
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:CreateBucket",
-          "s3:PutBucketVersioning",
-          "s3:DeleteObject",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:PutObject"
-        ],
-        "Resource" : [
-          "arn:aws:s3:::sst-asset-*"
-        ]
-      },
-      {
-        "Sid" : "ManageBootstrapECRRepo",
-        "Effect" : "Allow",
-        "Action" : [
-          "ecr:CreateRepository",
-          "ecr:DescribeRepositories"
-        ],
-        "Resource" : [
-          "arn:aws:ecr:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:repository/sst-asset"
-        ]
-      },
-      {
-        "Sid" : "ManageBootstrapSSMParameter",
-        "Effect" : "Allow",
-        "Action" : [
-          "ssm:GetParameter",
-          "ssm:GetParameters",
-          "ssm:PutParameter"
-        ],
-        "Resource" : [
-          "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/sst/passphrase/*",
-          "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/sst/bootstrap"
-        ]
-      },
-      {
-        "Sid" : "ManageApplicationProductionBucket",
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:CreateBucket",
-          "s3:PutBucketVersioning",
-          "s3:DeleteObject",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:PutObject",
-          "s3:GetObjectTagging",
-          "s3:PutObjectTagging"
-        ],
-        "Resource" : [
-          "arn:aws:s3:::ast-production-*",
-          "arn:aws:s3:::sst-asset-*"
-        ]
-      },
-      {
-        "Sid" : "ManageLambdaFunctions",
-        "Effect" : "Allow",
-        "Action" : [
-          "lambda:GetFunction",
-          "lambda:UpdateFunctionCode",
-          "lambda:ListVersionsByFunction",
-          "lambda:GetFunctionCodeSigningConfig",
-          "lambda:InvokeFunction",
-          "lambda:UpdateFunctionConfiguration"
-        ],
-        "Resource" : [
-          "*"
-        ]
-      },
-      {
-        "Sid" : "ManageCloudfrontDistributions",
-        "Effect" : "Allow",
-        "Action" : [
-          "cloudfront:CreateInvalidation"
-        ],
-        "Resource" : [
-          "*"
-        ]
-      }
-    ]
-  })
-}
-
-output "www_interactiveliterature_org_github_oidc_role" {
-  description = "Game Wrap deploy role ARN"
-  value       = aws_iam_role.www_interactiveliterature_org_deploy.arn
+output "www_interactiveliterature_org_smtp_url" {
+  sensitive = true
+  value = module.www_interactiveliterature_org_sst_github_deployment.smtp_url
 }
