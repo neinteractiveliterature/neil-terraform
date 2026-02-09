142a3adfab50b2a86f363aec3019b242c882dabf
Nat Budin <nbudin@actbluetech.com>
2026-02-08 18:45:38 -0800

    Remove unused outputs

--- main.tf
+++ main.tf
@@ -136,13 +136,3 @@ module "github-oidc" {
   repositories              = ["neinteractiveliterature/neil-terraform"]
   oidc_role_attach_policies = [aws_iam_policy.neil-terraform-state-read.arn]
 }
-
-output "oidc_provider_arn" {
-  description = "OIDC provider ARN"
-  value       = module.github-oidc.oidc_provider_arn
-}
-
-output "github_oidc_role" {
-  description = "CICD GitHub role."
-  value       = module.github-oidc.oidc_role
-}
